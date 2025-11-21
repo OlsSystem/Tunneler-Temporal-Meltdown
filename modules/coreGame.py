5fbf36091b6d9f307fef8359cbbf1279ef3030fa
OlsSystem <70857587+OlsSystem@users.noreply.github.com>
2025-11-21 14:48:53 +0000

    (#1) Small bits of formatting

--- modules/coreGame.py
+++ modules/coreGame.py
@@ -6,11 +6,9 @@ import modules.utils.ImageButton as ImgButton
 # ---- Misc Variables ---- #
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-
 # ---- Initialising Variables ---- # 
 
 class CoreGame:
-    
     def __init__(self):
         self.screen = pygame.display.set_mode((800,500))
         self.isRunning = True
@@ -25,9 +23,7 @@ class CoreGame:
         startButton = ImgButton.ImageButton(100,200, pygame.image.load(os.path.join(ROOT_DIR, '../assets/Image.png')).convert_alpha(), 0.7, self.screen)
         
         while self.isRunning:
-            
             startButton.draw()
-            
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.isRunning = False
