27d6af68fc9f93582f824bf38d8c65f7765beed3
OlsSystem <70857587+OlsSystem@users.noreply.github.com>
2025-11-23 22:47:19 +0000

    (#1) Added buttons and button functionality

--- modules/coreGame.py
+++ modules/coreGame.py
@@ -2,29 +2,47 @@
 import pygame
 import os
 import modules.utils.ImageButton as ImgButton
-
+from modules.utils.TextButton import TextButton
+from modules.handTracking import TrackHands
 # ---- Misc Variables ---- #
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # ---- Initialising Variables ---- # 
 
 class CoreGame:
     def __init__(self):
-        self.screen = pygame.display.set_mode((800,500))
-        self.isRunning = True
-        
         pygame.init()
         
+        self.screen = pygame.display.set_mode((800,500))
+        self.isRunning = True
+        self.HT = TrackHands()
+                
         self.startUp()
         
         
     def startUp(self):
-                
-        startButton = ImgButton.ImageButton(100,200, pygame.image.load(os.path.join(ROOT_DIR, '../assets/Image.png')).convert_alpha(), 0.7, self.screen)
+        
+        startButton = TextButton(100, 300, "Start", 38, (255,255,255), self.screen)
+        endButton = TextButton(100, 100, "End", 38, (255,255,255), self.screen)
+
         
         while self.isRunning:
+            self.screen.fill((30,30,30))
             startButton.draw()
+            endButton.draw()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.isRunning = False
                     pygame.quit()
+                    self.HT.stop()
+                    break
+                    
+                if startButton.isClicked(event):
+                    print('CLICKED START')
+                    self.HT.start()
+                    
+                if endButton.isClicked(event):
+                    print('CLICKED END')
+                    self.HT.stop()
+                    
+                    
+            pygame.display.update()
